540b640a5abf604ac35a17a85855c3fe89b19512
pedrorfs <pedrorfs11@gmail.com>
2024-09-04 23:14:26 -0300

    Update collect_user function

--- user_example.py
+++ user_example.py
@@ -23,17 +23,35 @@ def get_selected_attibutes(video):
         selected_data["duration"] = video["duration"]
     return selected_data
 
-async def collect_user(unique_id):
+async def get_comments(video):
+    comments = []
+    async for comment in video.comments(count=30):
+        if comment is None:
+            continue
+        comments.append(comment.as_dict)
+    return comments
+
+async def collect_user(unique_id, comments=False, path=None):
+    if path == None:
+        path = rf"./{unique_id}"
+    else:
+        path = path + rf"./{unique_id}"
+    os.mkdir(path)
+    
     async with TikTokApi() as api:
         await api.create_sessions(ms_tokens=[ms_token], num_sessions=1, sleep_after=3, headless=False)
-        user = api.user(unique_id)
-        # user_data = await user.info()
-        # print(user_data)
+        user = api.user(unique_id)           
+
         c = 1
         async for video in user.videos(count=30):
-            with open(f"./videos/sample{c}.json", "w") as outfile:
-                json.dump(get_selected_attibutes(video.as_dict), outfile)
-            c = c + 1
+            selected_data = get_selected_attibutes(video.as_dict)
+            try:
+                if comments:
+                    selected_data["comments"] = await get_comments(video)
+            finally:
+                with open(f"{path}/sample{c}.json", "w") as outfile:
+                    json.dump(selected_data, outfile)
+                c = c + 1
 
 if __name__ == "__main__":
     asyncio.run(user_example())
