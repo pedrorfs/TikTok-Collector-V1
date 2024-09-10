5b13f2033c4c88a1e476a3d63e9a504be55b428f
pedrorfs <pedrorfs11@gmail.com>
2024-09-10 00:06:11 -0300

    Add new functions on collector

--- collector.py
+++ collector.py
@@ -12,6 +12,7 @@ user = enviroment_variables["USER"]
 video = enviroment_variables["VIDEO"]
 path = enviroment_variables["OUTPUT_PATH"]
 token = enviroment_variables["TOKEN"]
+hashtag = enviroment_variables["HASHTAG"]
 
 async def main():
     if mode == "User":
@@ -19,6 +20,12 @@ async def main():
 
     elif mode == "Video":
         await get_video_example()
+
+    elif mode == "Comments":
+        await collect_comments(unique_id=user, path=path)
+
+    elif mode == "Hashtag":
+        await collect_hashtag_videos(hashtag, path=path)
 
     elif mode == "Sound":
         await sound_videos()
@@ -40,8 +47,17 @@ def get_selected_attibutes(video):
         selected_data["duration"] = video["duration"]
     return selected_data
 
-def get_create_time(video):
+def get_video_create_time(video):
     return video["createTime"] 
+
+def get_comment_create_time(video):
+    return video["create_time"] 
+
+def get_comment_info(comment):
+    info = {}
+    info["text"] = comment.text
+    info["likes_count"] = comment.likes_count
+    return info 
 
 async def collect_user(unique_id, path=None):
     if path == None:
@@ -56,10 +72,50 @@ async def collect_user(unique_id, path=None):
 
         async for video in user.videos(count=30):
             selected_data = get_selected_attibutes(video.as_dict)
-            file_name = get_create_time(video.as_dict)
+            file_name = get_video_create_time(video.as_dict)
             with open(f"{path}/{file_name}.txt", "w", encoding='utf-8') as outfile:
                 outfile.write(str(selected_data))
                 # json.dump(selected_data, outfile)
+
+async def collect_hashtag_videos(hashtag, path=None):
+    if path == None:
+        path = rf"./{hashtag}"
+    else:
+        path = path + rf"./{hashtag}"
+    os.mkdir(path)
+    
+    async with TikTokApi() as api:
+        await api.create_sessions(ms_tokens=[token], num_sessions=1, sleep_after=3, headless=False)
+        tag = api.hashtag(name=hashtag)
+
+        async for video in tag.videos(count=30):
+            selected_data = get_selected_attibutes(video.as_dict)
+            file_name = get_video_create_time(video.as_dict)
+            with open(f"{path}/{file_name}.txt", "w", encoding='utf-8') as outfile:
+                outfile.write(str(selected_data))
+                # json.dump(selected_data, outfile)
+
+async def collect_comments(unique_id, path=None):
+    if path == None:
+        path = rf"./{unique_id}"
+    else:
+        path = path + rf"./{unique_id}"
+
+    os.mkdir(path)
+
+    async with TikTokApi() as api:
+        await api.create_sessions(ms_tokens=[token], num_sessions=1, sleep_after=3, headless=False)
+        user = api.user(unique_id)           
+
+        async for video in user.videos(count=30):
+            video_directory = rf"{path}/{video.id}"
+            os.mkdir(video_directory)
+
+            async for comment in video.comments(count=30):
+                comment_info = get_comment_info(comment)
+                file_name = get_comment_create_time(comment.as_dict)
+                with open(f"{video_directory}/{file_name}.txt", "w", encoding='utf-8') as outfile:
+                    outfile.write(str(comment_info))
 
 if __name__ == "__main__":
     asyncio.run(main())
